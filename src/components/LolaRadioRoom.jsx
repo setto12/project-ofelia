1cfbe649c64cb26ad0328e7c3327122efcc696d9
Seth Dayao <dayao.seth@gmail.com>
2026-05-08 08:47:07 +0800

    chore: layout update and asset updates

--- src/components/LolaRadioRoom.jsx
+++ src/components/LolaRadioRoom.jsx
@@ -3,27 +3,6 @@ import { useEffect, useRef, useState } from "react";
 export default function LolaRadioRoom() {
   const audioRef = useRef(null);
   const [isPlaying, setIsPlaying] = useState(false);
-  const [scale, setScale] = useState(1);
-
-  const ROOM_WIDTH = 640;
-  const ROOM_HEIGHT = 360;
-
-  useEffect(() => {
-    const updateScale = () => {
-      const newScale = Math.min(
-        window.innerWidth / ROOM_WIDTH,
-        window.innerHeight / ROOM_HEIGHT
-      );
-
-      setScale(newScale);
-    };
-
-    updateScale();
-
-    window.addEventListener("resize", updateScale);
-
-    return () => window.removeEventListener("resize", updateScale);
-  }, []);
 
   const toggleRadio = async () => {
     if (!audioRef.current) return;
@@ -43,45 +22,31 @@ export default function LolaRadioRoom() {
 
   return (
     <div className="screen">
-      <audio
-        ref={audioRef}
-        src="https://playerservices.streamtheworld.com/api/livestream-redirect/DZMMAAC_SC"
-      />
-
-      <div
-        className="room"
-        style={{
-          transform: `scale(${scale})`,
-        }}
-      >
-        {/* Background Wall */}
-        <div className="wall" />
-
-        {/* Floor */}
-        <div className="floor" />
-
-        {/* lola */}
-        <img
-          src="/lola.png"
-          alt="lola"
-          className="lola"
+        <audio
+            ref={audioRef}
+            src="https://playerservices.streamtheworld.com/api/livestream-redirect/DZMMAAC_SC"
         />
 
-        {/* Table */}
-        <img
-          src="/table.png"
-          alt="Table"
-          className="table"
-        />
+        <div className="room">
+            <img
+            src="/lola.png"
+            alt="lola"
+            className="lola"
+            />
 
-        {/* Radio */}
-        <img
-          src="/radio.png"
-          alt="Radio"
-          className={`radio ${isPlaying ? "playing" : ""}`}
-          onClick={toggleRadio}
-        />
-      </div>
-    </div>
+            <img
+            src="/table.png"
+            alt="Table"
+            className="table"
+            />
+
+            <img
+            src="/radio.png"
+            alt="Radio"
+            className={`radio ${isPlaying ? "playing" : ""}`}
+            onClick={toggleRadio}
+            />
+        </div>
+        </div>
   );
 }
